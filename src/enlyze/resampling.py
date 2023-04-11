7637059e1270031c87b1cf75ee0c7582fcb69eb1
Mohamed Attia <m.attia@enlyze.com>
2023-04-11 09:13:11 +0200

    `docs-fmt`.

--- src/enlyze/resampling.py
+++ src/enlyze/resampling.py
@@ -4,17 +4,16 @@ import enlyze.models as user_models
 def convert_to_variable_with_resampling_method(
     variable: user_models.Variable, resampling_method: user_models.ResamplingMethod
 ) -> user_models.VariableWithResamplingMethod:
-    """Convert a :class:`~enlyze.models.Variable` to a
-    :class:`~enlyze.models.VariableWithResamplingMethod`.
+    """Convert a :class:`~enlyze.models.Variable` to a :class:`~enlyze.models.VariableWithResamplingMethod`.
 
     :param variable: The variable to convert.
     :type variable: :class:`~enlyze.models.Variable`
-
     :param resampling_method: The resampling method to set on the variable.
     :type resampling_method: :class:`~enlyze.models.ResamplingMethod`
 
     :returns: A variable with resampling method.
     :rtype: :class:`~enlyze.models.VariableWithResamplingMethod`.
+
     """
 
     return user_models.VariableWithResamplingMethod(
