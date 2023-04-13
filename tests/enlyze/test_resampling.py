e2bd1c3947e59ccd49e62b2585bb3f309d7970de
Mohamed Attia <m.attia@enlyze.com>
2023-04-13 09:20:29 +0200

    Remove test that was parametrized in another one.

--- tests/enlyze/test_resampling.py
+++ tests/enlyze/test_resampling.py
@@ -42,40 +42,11 @@ from enlyze.validators import VARIABLE_ARRAY_DATA_TYPES
         ),
     ],
 )
-def test_convert_to_variable_with_resampling_method(data, variable_strategy, resampling_method_strategy):
+def test_convert_to_variable_with_resampling_method(
+    data, variable_strategy, resampling_method_strategy
+):
     variable = data.draw(variable_strategy)
     resampling_method = data.draw(resampling_method_strategy)
-    variable_with_resampling_method = convert_to_variable_with_resampling_method(
-        variable, resampling_method
-    )
-
-    variable_with_resampling_method_dict = asdict(variable_with_resampling_method)
-    variable_resampling_method = variable_with_resampling_method_dict.pop(
-        "resampling_method"
-    )
-
-    assert asdict(variable) == variable_with_resampling_method_dict
-    assert variable_resampling_method == resampling_method
-
-
-@given(
-    variable=st.builds(
-        Variable,
-        data_type=st.sampled_from((VariableDataType.BOOLEAN, VariableDataType.STRING)),
-    ),
-    resampling_method=st.sampled_from(
-        (
-            ResamplingMethod.FIRST,
-            ResamplingMethod.LAST,
-            ResamplingMethod.MAX,
-            ResamplingMethod.MIN,
-            ResamplingMethod.COUNT,
-        )
-    ),
-)
-def test_convert_to_variable_with_resampling_method_boolean_or_string(
-    variable, resampling_method
-):
     variable_with_resampling_method = convert_to_variable_with_resampling_method(
         variable, resampling_method
     )
