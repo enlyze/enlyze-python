4d7821d494a5e94b0a4e1342a8664c8b8fc37ba7
Deniz Saner <d.saner@enlyze.com>
2023-09-29 14:43:28 +0200

    move string_model fixture to api_clients/conftest

---
+++ tests/enlyze/api_clients/conftest.py
@@ -0,0 +1,14 @@
+from unittest.mock import patch
+
+import pytest
+
+from enlyze.api_clients.base import ApiBaseModel
+
+
+@pytest.fixture
+def string_model():
+    with patch(
+        "enlyze.api_clients.base.ApiBaseModel.parse_obj",
+        side_effect=lambda o: str(o),
+    ):
+        yield ApiBaseModel
