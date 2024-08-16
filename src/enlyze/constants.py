867fc26220079fe73d6f8c6b09fa08ba3de59795
Deniz Saner <d.saner@enlyze.com>
2024-08-16 11:25:05 +0200

    remove api_clients packages

--- src/enlyze/constants.py
+++ src/enlyze/constants.py
@@ -4,12 +4,6 @@ ENLYZE_BASE_URL = "https://app.enlyze.com"
 #: URL sub-path of the ENLYZE platform API.
 PLATFORM_API_SUB_PATH = "api/v2/"
 
-#: URL sub-path where the Timeseries API is deployed on the ENLYZE platform.
-TIMESERIES_API_SUB_PATH = "api/timeseries/v1/"
-
-#: URL sub-path where the Production Runs API is deployed on the ENLYZE platform.
-PRODUCTION_RUNS_API_SUB_PATH = "api/production-runs/v1/"
-
 #: HTTP timeout for requests to the Timeseries API.
 #:
 #: Reference: https://www.python-httpx.org/advanced/timeouts/
