6a35b73da304bb1ac2384289ce9c0b5fa3f38325
Daniel Roussel <d.roussel@enlyze.com>
2024-10-01 10:54:25 +0200

    fix docs and give recommendation how to resolve error

--- src/enlyze/errors.py
+++ src/enlyze/errors.py
@@ -34,5 +34,7 @@ class DuplicateDisplayNameError(EnlyzeError):
     """Variables with duplicate display names
 
     Resolving variable UUIDs to display names would result in ambiguity because
-    multiple variables have the same display name.
+    multiple variables have the same display name. You should either fix the
+    duplicate variable display names via the ENLYZE App or don't request them at
+    the same time.
     """
